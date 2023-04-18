c8c533fe494e8952ad38dfd9b7ec568522dba321
israel kalish <1234kalish@gmail.com>
2023-04-18 22:41:42 +0300

    memoryGame

--- shiureyJs/dom/memoryGame/mein.js
+++ shiureyJs/dom/memoryGame/mein.js
@@ -1,4 +1,3 @@
-console.log("card")
 const NewPlaceForTheCard = ()=> {
     let i =Math.floor(Math.random()*cardsArray.length)
     if (!cardsOfDisPlay[i]){return i} else {return NewPlaceForTheCard()}
@@ -23,21 +22,17 @@ for (let index = 0; index < amountOfTickets/2; index++) {
     cardsOfDisPlay[NewPlaceForTheCard()]= newPlace;
     cardsOfDisPlay[NewPlaceForTheCard()]= newPlace;
 }
-console.log(cardsOfDisPlay)
 
 let lastCard =[0];
-// inner.
-console.log(inner)
 let lastClick =0 
 for (let index = 0; index < inner.length; index++) {
     inner[index].addEventListener("click", function(){
         let d = new Date();
         let t = d.getTime();
-        if(t - lastClick < 1700){return}console.log(t - lastClick)
+        if(t - lastClick < 1300){return}
         inner[index].innerHTML=`\n<p class="front">${cardsOfDisPlay[index]};</p>\n<p class="back back1"></p>\n`
         lastCard[0] ++
         lastCard.push(inner[index])
-        console.log(lastCard[1].innerText)
         if(lastCard[2]){
             if(lastCard[1].innerText===lastCard[2].innerText){
                 lastCard[0] = 0
@@ -47,11 +42,10 @@ for (let index = 0; index < inner.length; index++) {
             lastClick = t;
             setTimeout(function() {
             lastCard[1].innerHTML=`\n<p class="front">&#10068;</p>\n<p class="back back1"></p>\n`
-            console.log(lastCard)
             lastCard[2].innerHTML=`\n<p class="front">&#10068;</p>\n<p class="back back1"></p>\n`
             lastCard[0] = 0
             lastCard.pop();lastCard.pop();
-            }, 1700)
+            }, 1300)
         }
     })
 }
