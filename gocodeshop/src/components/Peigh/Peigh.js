b4c2b72d65746893050b3abc2ea628ea586577a9
israel kalish <1234kalish@gmail.com>
2023-05-08 20:16:35 +0300

    with 0501

---
+++ gocodeshop/src/components/Peigh/Peigh.js
@@ -0,0 +1,10 @@
+import { ProductFilter } from "../ProductFilter/ProductFilter"
+import { Products } from "../Products/Products"
+
+export const Peigh = () => {
+  return(
+      <div>
+      </div>
+  )
+}
+
--- gocodeshop/src/components/ProductCard/ProductCard.js
+++ gocodeshop/src/components/ProductCard/ProductCard.js
@@ -1,16 +1,20 @@
 import './ProductCard.css'
 
 export const ProductCard = ({src,title,price}) => {
+  const AmountOfItemsToUpload = 0
     return(
         <div className="product-card">
           <div className="product-image">
-            <img
-              src={src}
-            />
+            <img src={src}/>
           </div>
           <div className="product-info">
             <h5>{title}</h5>
             <h6>${price}</h6>
+            <div>
+              <button></button>
+              <P>{AmountOfItemsToUpload}</P>
+              <button></button>
+            </div>
           </div>
         </div>
     )
